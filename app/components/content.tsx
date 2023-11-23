7749a480b29c8ce93300b7b5bda155ebfde834ed
wangyong220531 <wangyong220531@gmail.com>
2023-11-23 19:31:52 +0800

    202311231931update

--- app/components/content.tsx
+++ app/components/content.tsx
@@ -4,8 +4,8 @@ import RightSide from "./rightSide";
 export default function Content() {
     return (
         <div className="absolute pt-6 w-10/12 left-1/2 top-20 transform -translate-x-1/2 flex gap-x-2.5">
-            <QuestionContainer />
             <RightSide />
+            <QuestionContainer />
         </div>
     )
 }
--- app/components/rightSide.tsx
+++ app/components/rightSide.tsx
@@ -1,5 +1,76 @@
+import { useState } from "react"
+
+export interface SortItemChild {
+    key: number,
+    content: string
+}
+
+export interface SortItem {
+    id: number
+    label: string
+    children: SortItemChild[]
+}
+
+const sortList: SortItem[] = [
+    {
+        id: 0,
+        label: "排序",
+        children: [
+            {
+                key: 0,
+                content: "按关注数量"
+            },
+            {
+                key: 1,
+                content: "按收藏数量"
+            },
+            {
+                key: 2,
+                content: "按留言数量"
+            }
+        ]
+    },
+    {
+        id: 1,
+        label: "筛选",
+        children: [
+            {
+                key: 0,
+                content: "近一周内"
+            },
+            {
+                key: 1,
+                content: "近一月内"
+            },
+            {
+                key: 2,
+                content: "近一年内"
+            }
+        ]
+    }
+]
+
 export default function RightSide() {
+
+    // const [activedChild, setActivedChild] = useState(0)
+    const [sortActivedChild, setSortActivedChild] = useState(0)
+    const [filterActivedChild, setFilterActivedChild] = useState(0)
+
     return (
-        <div className="w-3/12 shadow-sm bg-white"></div>
+        <div className="w-2/12 bg-white shadow-sm">
+            {sortList.map((sortItem: SortItem) => {
+                return (
+                    <div className="flex flex-col gap-y-2" key={sortItem.id}>
+                        <div className="text-2xl font-bold px-6 pt-5 pb-3 border-b-[1px] border-gray-300 cursor-pointer">{sortItem.label}</div>
+                        {sortItem.id === 0 && <div className="flex flex-col gap-y-2 px-3 py-3">
+                            {sortItem.children.map((child: SortItemChild) => <div key={child.key} onClick={() => setSortActivedChild(child.key)} className={`px-6 py-2 text-xl rounded cursor-pointer ${sortActivedChild === child.key ? 'text-blue-600 font-bold bg-blue-100' : 'text-gray-500'}`}>{child.content}</div>)}
+                        </div>}
+                        {sortItem.id === 1 && <div className="flex flex-col gap-y-2 px-3 py-3">
+                            {sortItem.children.map((child: SortItemChild) => <div key={child.key} onClick={() => setFilterActivedChild(child.key)} className={`px-6 py-2 text-xl rounded cursor-pointer ${filterActivedChild === child.key ? 'text-blue-600 font-bold bg-blue-100' : 'text-gray-500'}`}>{child.content}</div>)}
+                        </div>}
+                    </div>
+                )
+            })}
+        </div>
     )
 }
