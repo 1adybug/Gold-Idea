cd7b79b867dfa4bfa91e8053023f2b91179f0b2e
wangyong220531 <wangyong220531@gmail.com>
2023-11-24 09:53:28 +0800

    202311240953update

--- app/detail/[id]/page.tsx
+++ app/detail/[id]/page.tsx
@@ -1,90 +1,17 @@
-"use client"
-import { Fragment, useState } from "react"
+import { Fragment } from "react"
 import Avator from "../../../assets/avator.jpg"
 import { StaticImageData } from "next/image"
 import Image from "next/image"
 import Header from "../../../components/header"
 import Comments, { CommentItem } from "../../../components/comments"
 import { PageProps } from "../../../.next/types/app/layout"
+import DetailFirstSection from "../../../components/detailFirstSection"
+import DetailSecondSection from "../../../components/detailSecondSection"
 
 export interface Unit {
     id: string
     unitNo: string
     unitName: string
-}
-
-export interface User {
-    userId: string
-    avator: StaticImageData
-    userName: string
-    policeNo: string
-    phone: string,
-    unit: string
-}
-
-export interface QuestionDetail {
-    id: string
-    content: string
-    goal: string
-    createTime: string
-    quizzer: User,
-    comments: CommentItem[]
-}
-
-export function DetailFirstSection(props: QuestionDetail) {
-
-    const { content, quizzer, createTime, goal } = props
-
-    return (
-        <div className="w-[1200px] h-full p-10 rounded bg-white shadow-sm flex flex-col gap-y-10">
-            <div className="flex flex-col gap-y-4">
-                <div className="text-3xl font-semibold">{content}</div>
-                <div className="flex gap-x-4 items-center text-xl text-gray-600">
-                    <div className="flex gap-x-2.5 items-center">
-                        <Image src={quizzer.avator} alt="创建者头像" width={36} height={36} className="rounded" />
-                        <div className="text-black font-medium">{quizzer.userName}</div>
-                    </div>
-                    <div className="flex gap-x-2.5 items-center text-gray-400">
-                        <div>警号：{quizzer.policeNo}</div>
-                        <div>所属单位：{quizzer.unit}</div>
-                        <div>发表时间：{createTime}</div>
-                    </div>
-                </div>
-            </div>
-            <div className="">
-                <div className="text-2xl text-gray-600 border-l-[8px] border-blue-600 pl-3">{goal}</div>
-            </div>
-        </div>
-    )
-}
-
-export interface DetailSecondSectionProps {
-    userDemo: User
-    question: QuestionDetail
-}
-
-export function DetailSecondSection(props: DetailSecondSectionProps) {
-
-    const { userDemo, question } = props
-
-    const [inputedValue, setInputedValue] = useState("")
-
-    return (
-        <div className="w-[1200px] h-full flex flex-col gap-y-6 p-10 rounded bg-white shadow-sm">
-            <div className="text-2xl text-black font-semibold">评论&nbsp;&nbsp;{question.comments.length}</div>
-            <div className="flex gap-x-6">
-                <Image src={userDemo.avator} alt="用户头像" width={56} className="rounded h-[56px]" />
-                <div className="w-full flex flex-col gap-y-10 bg-gray-100 p-3 rounded-md">
-                    <textarea className="h-[200px] text-2xl bg-gray-100 focus:outline-none" placeholder="发表一条评论吧！" />
-                    <div className="flex items-center gap-x-6 ml-auto">
-                        <div className="text-2xl text-gray-400">{inputedValue.length}/1000</div>
-                        <button disabled={true} className="w-[110px] h-[50px] flex justify-center items-center text-white text-xl rounded bg-blue-600">发送</button>
-                    </div>
-                </div>
-            </div>
-            <Comments comments={question.comments} />
-        </div>
-    )
 }
 
 const userDemo = {
@@ -143,7 +70,6 @@ const questionDemo = {
 export default function Page({ params }: PageProps) {
 
     const { id } = params
-    const [question, setQuestion] = useState<QuestionDetail>(questionDemo)
 
     function handleAvatorClick() {
 
@@ -157,8 +83,8 @@ export default function Page({ params }: PageProps) {
         <Fragment>
             <Header isHomePage={false} />
             <div className="w-full h-scree flex flex-col gap-y-10 justify-center items-center pt-[110px]">
-                <DetailFirstSection id={question.id} content={question.content} goal={question.goal} createTime={question.createTime} quizzer={question.quizzer} comments={question.comments} />
-                <DetailSecondSection userDemo={userDemo} question={question} />
+                <DetailFirstSection id={questionDemo.id} content={questionDemo.content} goal={questionDemo.goal} createTime={questionDemo.createTime} quizzer={questionDemo.quizzer} comments={questionDemo.comments} />
+                <DetailSecondSection userDemo={userDemo} question={questionDemo} />
             </div>
         </Fragment>
     )
---
+++ components/detailSecondSection.tsx
@@ -0,0 +1,34 @@
+"use client"
+import { useState } from "react"
+import Comments from "./comments"
+import Image from "next/image"
+import { QuestionDetail, User } from "./detailFirstSection"
+
+export interface DetailSecondSectionProps {
+    userDemo: User
+    question: QuestionDetail
+}
+
+export default function DetailSecondSection(props: DetailSecondSectionProps) {
+
+    const { userDemo, question } = props
+
+    const [inputedValue, setInputedValue] = useState("")
+
+    return (
+        <div className="w-[1200px] h-full flex flex-col gap-y-6 p-10 rounded bg-white shadow-sm">
+            <div className="text-2xl text-black font-semibold">评论&nbsp;&nbsp;{question.comments.length}</div>
+            <div className="flex gap-x-6">
+                <Image src={userDemo.avator} alt="用户头像" width={56} className="rounded h-[56px]" />
+                <div className="w-full flex flex-col gap-y-10 bg-gray-100 p-3 rounded-md">
+                    <textarea className="h-[200px] text-2xl bg-gray-100 focus:outline-none" placeholder="发表一条评论吧！" />
+                    <div className="flex items-center gap-x-6 ml-auto">
+                        <div className="text-2xl text-gray-400">{inputedValue.length}/1000</div>
+                        <button disabled={true} className="w-[110px] h-[50px] flex justify-center items-center text-white text-xl rounded bg-blue-600">发送</button>
+                    </div>
+                </div>
+            </div>
+            <Comments comments={question.comments} />
+        </div>
+    )
+}
