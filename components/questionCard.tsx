0e9699159bb14ce5463a92ef836b5473ea418b5a
wangyong220531 <wangyong220531@gmail.com>
2023-12-06 17:14:02 +0800

    202312061713update

--- components/questionCard.tsx
+++ components/questionCard.tsx
@@ -54,6 +54,7 @@ export default function QuestionCard(props: QuestionCardProps) {
     }
 
     async function handleCollect(event: React.MouseEvent<HTMLDivElement>) {
+        event.stopPropagation()
         event.preventDefault()
         const res = await collectQuestion(id, userInfo.id, isCollected)
         if (!res) return
@@ -70,6 +71,7 @@ export default function QuestionCard(props: QuestionCardProps) {
     }
 
     async function handleAttention(event: React.MouseEvent<HTMLDivElement>) {
+        event.stopPropagation()
         event.preventDefault()
         const res = await attentionQuestion(id, userInfo.id, isAttentioned)
         if (!res) return
@@ -86,46 +88,42 @@ export default function QuestionCard(props: QuestionCardProps) {
     }
 
     function handleLinkClick() {
-        NProgress.start();
+        NProgress.start()
         NProgress.configure({
             minimum: 0.1,
             showSpinner: false,
             easing: 'ease',
             speed: 500,
-            trickle: false,
-            trickleSpeed: 200
         });
     }
 
     return (
-        <Link className="w-full h-full bg-white text-black p-8 cursor-pointer" href={`/detail/${id}`}>
-            <div onClick={handleLinkClick} className="flex flex-col gap-y-8">
-                <div className="flex flex-col gap-y-4">
-                    <div className="text-2xl font-semibold text-stone-800">{content}</div>
-                    <div className="flex items-center gap-x-2 text-gray-500">
-                        <div>发布者：{publisher.userName}</div>
-                        <div>警号：{publisher.policeNo}</div>
-                        <div>单位：{publisher.unit.unitName}</div>
-                        <div>手机号：{publisher.phone}</div>
-                    </div>
+        <Link className="w-full h-full bg-white text-black p-8 cursor-pointer flex flex-col gap-y-8" onClick={handleLinkClick} href={`/detail/${id}`}>
+            <div className="flex flex-col gap-y-4">
+                <div className="text-2xl font-semibold text-stone-800">{content}</div>
+                <div className="flex items-center gap-x-2 text-gray-500">
+                    <div>发布者：{publisher.userName}</div>
+                    <div>警号：{publisher.policeNo}</div>
+                    <div>单位：{publisher.unit.unitName}</div>
+                    <div>手机号：{publisher.phone}</div>
                 </div>
-                <div className="flex flex-wrap text-xl text-zinc-600 line-clamp-2 overflow-hidden">
-                    {goal}
+            </div>
+            <div className="flex flex-wrap text-xl text-zinc-600 line-clamp-2 overflow-hidden">
+                {goal}
+            </div>
+            <div className="mt-auto flex gap-x-6 items-center text-xl text-gray-500">
+                <div className="flex gap-x-1 items-center cursor-pointer">
+                    <Image src={Quote} alt={"引用"} width={26} height={18} className="transform rotate-225 scale-x-[-1]" />
+                    <div>{referCount}条引用</div>
                 </div>
-                <div className="mt-auto flex gap-x-6 items-center text-xl text-gray-500">
-                    <div className="flex gap-x-1 items-center cursor-pointer">
-                        <Image src={Quote} alt={"引用"} width={26} height={18} className="transform rotate-225 scale-x-[-1]" />
-                        <div>{referCount}条引用</div>
-                    </div>
-                    <div>发布时间：{advanceTime(createTime)}</div>
-                    <div className="flex gap-x-1 items-center transform -translate-y-[1px] cursor-pointer" onClick={handleCollect}>
-                        <Image src={isCollected ? CollectedIcon : DefaultCollectionIcon} alt={"收藏图标"} width={24} height={14} />
-                        <div className={isCollected ? "text-blue-600" : ""}>{currentCollectionsCount}</div>
-                    </div>
-                    <div className="flex gap-x-1 items-center cursor-pointer">
-                        <Image src={isAttentioned ? AttentionedIcon : UnAttentionedIcon} alt={"关注图标"} width={24} height={14} onClick={handleAttention} />
-                        <div className={isAttentioned ? "text-blue-600" : ""}>{currentAttentionsCount}</div>
-                    </div>
+                <div>发布时间：{advanceTime(createTime)}</div>
+                <div className="flex gap-x-1 items-center transform -translate-y-[1px] cursor-pointer" onClick={handleCollect}>
+                    <Image src={isCollected ? CollectedIcon : DefaultCollectionIcon} alt={"收藏图标"} width={24} height={14} />
+                    <div className={isCollected ? "text-blue-600" : ""}>{currentCollectionsCount}</div>
+                </div>
+                <div className="flex gap-x-1 items-center cursor-pointer">
+                    <Image src={isAttentioned ? AttentionedIcon : UnAttentionedIcon} alt={"关注图标"} width={24} height={14} onClick={handleAttention} />
+                    <div className={isAttentioned ? "text-blue-600" : ""}>{currentAttentionsCount}</div>
                 </div>
             </div>
         </Link>
